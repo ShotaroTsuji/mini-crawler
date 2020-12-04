fe015461393b09d95410452470bc79576f4c61b2
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2020-12-04 21:43:25 +0900

    Make relative URLs absolute

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use select::document::Document;
 use select::predicate::Name;
+use url::Url;
+use url::ParseError as UrlParseError;
 
 fn main() -> eyre::Result<()> {
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?
-        .text()?;
+    let response = reqwest::blocking::get("https://www.rust-lang.org")?;
+    let base_url = response.url().clone();
+    let body = response.text()?;
     let doc = Document::from(body.as_str());
 
     for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
-        println!("{}", href);
+        match Url::parse(href) {
+            Ok(url) => { println!("{}", url); },
+            Err(UrlParseError::RelativeUrlWithoutBase) => {
+                let url = base_url.join(href)?;
+                println!("{}", url);
+            },
+            Err(e) => { println!("Error: {}", e); },
+        }
     }
 
     Ok(())
