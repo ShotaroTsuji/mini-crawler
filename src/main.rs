49b94e420300e075d8cbc6ff94dbc84f69109e11
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2020-12-04 21:27:43 +0900

    Add extracting links from an HTML document

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use select::document::Document;
+use select::predicate::Name;
+
 fn main() -> eyre::Result<()> {
     let body = reqwest::blocking::get("https://www.rust-lang.org")?
         .text()?;
+    let doc = Document::from(body.as_str());
 
-    println!("body = {:?}", body);
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+        println!("{}", href);
+    }
 
     Ok(())
 }
