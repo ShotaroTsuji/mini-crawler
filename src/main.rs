02285e2b5358425fedc529553f355d6db0cf0ab9
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2020-12-04 17:43:30 +0900

    Impelemnt getting an HTML page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> eyre::Result<()> {
+    let body = reqwest::blocking::get("https://www.rust-lang.org")?
+        .text()?;
+
+    println!("body = {:?}", body);
+
+    Ok(())
 }
